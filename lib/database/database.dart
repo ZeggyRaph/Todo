342cc907c005ff7e0349a2519e99ed280fb110e5
ZeggyRaph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 <raphaelzegenee@gmail.com>
2023-09-06 11:44:27 +0100

    Implemented functions to read, write, update & delete data from database

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:my_todo/models/note_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -39,5 +40,58 @@ class DatabaseHelper{
           '$colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colPriority TEXT, $colDate TEXT, $colStatus INTEGER )'
     );
   }
+
+  //Creating functions that will interact with the database
+Future<List<Map<String, dynamic>>> getNoteMapList() async{
+  Database?   db = await this.db;
+  final List<Map<String, dynamic>> result = await db!.query(noteTable);
+  return result;
+}
+
+Future<List<Note>> getNoteList() async{
+    final List<Map<String, dynamic>> noteMapList = await getNoteMapList();
+    final List<Note> noteList = [];
+    for (var noteMap in noteMapList) {
+      noteList.add(Note.fromMap(noteMap));
+    }
+    noteList.sort((noteA, noteB) => noteA.date!.compareTo(noteB.date!));
+    return noteList;
+}
+
+// This function will insert data into the database
+Future<int> insertNote(Note note) async {
+    Database? db = await this.db;
+    final int result = await db!.update(
+        noteTable, note.toMap(),
+      where: '$colId = ?',
+      whereArgs: [note.id],
+    );
+    return result;
+}
+
+
+// This function will update data into the database
+  Future<int> updateNote(Note note) async {
+    Database? db = await this.db;
+    final int result = await db!.update(
+      noteTable, note.toMap(),
+      where: '$colId = ?',
+      whereArgs: [note.id],
+    );
+    return result;
+  }
+
+  // This function will delete data from the database
+  Future<int> deleteNote(int id) async {
+    Database? db = await this.db;
+    final int result = await db!.delete(
+      noteTable,
+      where: '$colId = ?',
+      whereArgs: [id],
+    );
+    return result;
+  }
+
+
 
 }
